ebbf9835f9cd911f3ccd5dd4376fff1121374c5c
yohcho <95256337+yohcho@users.noreply.github.com>
2022-09-24 05:56:15 -0400

    Improvements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
 import { useState } from 'react';
 
+import startFlag from "./assets/start.png"
+import endFlag from "./assets/end.png"
+import wall from "./assets/wall.png"
+
 function App() {
   const [maze,setMaze] = useState(Array.from({length: 22},()=> Array.from({length: 56}, () => "none")))
   const [algorithm,setAlgorithm] = useState("dfs")
   const [selection,setSelection] = useState("null")
+  const [valid,setValid] = useState(true)
 
   const [startPos,setStartPos] = useState([])
   const [endPos,setEndPos] = useState([])
   const [walls, setWalls] = useState([])
+
+  const [holdMouse,setHoldMouse] = useState(false)
 
   const finalPath = []
   const topBar=()=>{
@@ -37,27 +44,22 @@ function App() {
     const searchList = [startPos]
     const allSearch = []
     let current = []
-    while(searchList.length>0){
-      current = algorithm==="bfs" ? searchList.shift() : searchList.pop()
-      allSearch.push(current)
-      if(addNeighbors(searchList,paths,current)) break
-    }
-
-    let searchIndex = 0;
     const searchInterval = setInterval(()=>{
-                      const tile = allSearch[searchIndex]
-                      if(searchIndex===allSearch.length-1 || (tile[0]===endPos[0] && tile[1]===endPos[1])){
-                        clearInterval(searchInterval)
-                        finishUp(current,paths)
-                      }
-                      changeColor(tile,"searchedPath")
-                      searchIndex++
-                    },1)
+      current = algorithm==="bfs" ? searchList.shift() : searchList.pop()
+      console.log(current)
+      changeColor(current,"searchedPath")
+      allSearch.push(current)
+      if(addNeighbors(searchList,paths,current) || searchList.length===0){
+        clearInterval(searchInterval)
+        finishUp(current,paths)
+      }
+    },1)    
   }
 
   const finishUp=(current,paths)=>{
+    console.log(current)
     if(!(current[0]===endPos[0] && current[1]===endPos[1])){
-      alert("No possible way")
+      setValid(false)
       return
     }
     while(!(current[0]===startPos[0] && current[1]===startPos[1])){
@@ -75,12 +77,12 @@ function App() {
     finalPath.unshift(startPos)
     let index = 0;
     const interval = setInterval(()=>{
-                      if(index===finalPath.length-1)
-                        clearInterval(interval)
-                      const tile = finalPath[index]
-                      changeColor(tile,"finalPath")
-                      index++
-                    },25)
+      if(index===finalPath.length-1)
+        clearInterval(interval)
+      const tile = finalPath[index]
+      changeColor(tile,"finalPath")
+      index++
+    },25)
   }
 
   const changeColor=(tile,value)=>{
@@ -102,6 +104,7 @@ function App() {
     for(let i =0;i<4;i++){
       if(checkValid(nodesToCheck[i],paths,i)){
         searchList.push(nodesToCheck[i])
+        changeColor(nodesToCheck[i],"nextSearchPath")
         paths[nodesToCheck[i][0]][nodesToCheck[i][1]] = directions[i]
       }
     }
@@ -123,15 +126,16 @@ function App() {
       return
     else if(changeTo==="🏁"){
       setEndPos([row,col])
+      setSelection("null")
       return
     }
     else if(changeTo==="🚩"){
       setStartPos([row,col])
+      setSelection("null")
       return
     }
     else if(changeTo==="🧱"){
       const text = `${row}-${col}`
-      console.log(text)
       setWalls(prevWalls=>{
         const newWalls=[...prevWalls]
         if(newWalls.includes(text))
@@ -151,19 +155,28 @@ function App() {
       for(let j = 0;j<maze[0].length;j++){
         let content = ""
         if(startPos[0]===i && startPos[1]===j)
-          content = "🚩"
+          content = startFlag
         if(endPos[0]===i && endPos[1]===j)
-          content = "🏁"
+          content = endFlag
         if(walls.includes(`${i}-${j}`))
-          content = "🧱"
+          content = wall
         let bg = "transparent"
         if(maze[i][j]==="finalPath")
           bg="red"
         if(maze[i][j]==="searchedPath")
           bg="green"
+        if(maze[i][j]==="nextSearchPath")
+          bg="cyan"
+        const styling = {
+          backgroundColor:bg,
+          backgroundImage: `url(${content})`
+        }
         row.push(
-          <div style={{backgroundColor:bg}} className='maze-row-tile' onClick={()=>handleTileClicked(i,j,selection)}>
-            {content}
+          <div 
+            style={styling} 
+            className='maze-row-tile' 
+            onMouseEnter={()=>{holdMouse && handleTileClicked(i,j,selection)}} 
+            onClick={()=>handleTileClicked(i,j,selection)}>
           </div>
         )
       }
@@ -176,12 +189,26 @@ function App() {
     cursor:`url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='20' height='20'><rect rx='20' width='100%' height='100%' fill='transparent'/><text x='50%' y='50%' font-family='sans-serif' font-weight='bold' font-size='22' dominant-baseline='middle' text-anchor='middle' fill='white'>${selection}</text></svg>") 16 0, auto`
   }
 
+  const createPopUp=()=>{
+    if(valid)
+      return
+    return (
+      <div className='maze-popup'>
+        There is no possible route!
+        <button className='maze-popup-close' onClick={()=>{setValid(true)}}>X</button>
+      </div>
+    )
+  }
+
   return (
     <div 
+      onMouseDown={()=>{setHoldMouse(true)}}
+      onMouseUp={()=>{setHoldMouse(false)}}
       style={selection!=="null" ? cursorStyle : {}} 
       className='maze'>
       {topBar()}
       {createDisplay()}
+      {createPopUp()}
     </div>
   );
 }
